9edb453a45d4f7a15a6a99d59fb03abb8edef617
Anastasia Mela <amela@programize.com>
2025-01-24 20:33:11 +0200

    fix error in init.sql

--- db-scripts/init.sql
+++ db-scripts/init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS quotes (
+CREATE TABLE IF NOT EXISTS quote (
     id INT AUTO_INCREMENT PRIMARY KEY,
     text TEXT NOT NULL,
     author VARCHAR(255)
 );
 
-INSERT INTO quotes (author, text) VALUES ('Oscar Wilde', 'Be yourself; everyone else is already taken.');
-INSERT INTO quotes (author, text) VALUES ('Albert Einstein', 'Life is like riding a bicycle.');
+INSERT INTO quote (author, text) VALUES ('Oscar Wilde', 'Be yourself; everyone else is already taken.');
+INSERT INTO quote (author, text) VALUES ('Albert Einstein', 'Life is like riding a bicycle.');
 
 
