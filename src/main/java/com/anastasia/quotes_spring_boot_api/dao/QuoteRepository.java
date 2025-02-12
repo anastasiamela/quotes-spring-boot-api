d930de3fa2b71e215f625459fa81da0da701ca68
anastasiamela <anastasia.mela11@gmail.com>
2025-02-12 17:11:10 +0200

    add @Repository

--- src/main/java/com/anastasia/quotes_spring_boot_api/dao/QuoteRepository.java
+++ src/main/java/com/anastasia/quotes_spring_boot_api/dao/QuoteRepository.java
@@ -3,9 +3,11 @@ package com.anastasia.quotes_spring_boot_api.dao;
 import com.anastasia.quotes_spring_boot_api.model.entity.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface QuoteRepository extends JpaRepository<Quote, Integer> {
 
     @Query("select q from Quote q where q.text like %:text%")
