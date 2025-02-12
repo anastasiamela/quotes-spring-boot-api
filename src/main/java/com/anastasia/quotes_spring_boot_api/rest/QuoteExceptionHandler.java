96ae27670ca7313bd37a841971e9455d1ed5af7c
anastasiamela <anastasia.mela11@gmail.com>
2025-02-12 16:43:08 +0200

    override handleValidationExceptions

--- src/main/java/com/anastasia/quotes_spring_boot_api/rest/QuoteExceptionHandler.java
+++ src/main/java/com/anastasia/quotes_spring_boot_api/rest/QuoteExceptionHandler.java
@@ -3,10 +3,14 @@ package com.anastasia.quotes_spring_boot_api.rest;
 import com.anastasia.quotes_spring_boot_api.model.exceptions.QuoteBadRequestException;
 import com.anastasia.quotes_spring_boot_api.model.dto.QuoteErrorResponse;
 import com.anastasia.quotes_spring_boot_api.model.exceptions.QuoteNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
@@ -29,16 +33,20 @@ public class QuoteExceptionHandler extends ResponseEntityExceptionHandler {
         return new QuoteErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
         Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getAllErrors().forEach((error) -> {
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return errors;
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
